ff63a2d3b434cca099ec24850677231444e6641f
FadySalah28 <salahfady28@gmail.com>
2026-08-31 20:37:22 +0300

    Add grading system in lesson3.dart

---
+++ lesson3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int score =40 ;
+  if (score >=90) {
+    print('ممتاز! A');
+  } else if (score>= 75) {
+    print('جيد جدا! B'); 
+  } else if (score >= 50) {
+    print('مقبول! c');
+  } else {
+    print('راسب، محتاج تذاكر اكتر');}
+}
